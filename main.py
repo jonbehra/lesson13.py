6cf78992c98037833e9fe73e10952d6c5db14846
jonbehra <165320382+jonbehra@users.noreply.github.com>
2025-06-03 20:34:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 print(np.__version__)
 
 array_2d = np.array([[1,2,3,4,5],[6,7,8,9,10]])
@@ -22,3 +24,33 @@ print(sum_columns)
 
 sum_rows = np.sum(array_2d, axis = 1)
 print(sum_rows)
+
+
+#PANDAS
+
+products = ['apples','bananas','orange','grapes']
+sales = [150,200,180,90]
+
+sales_series = pd.Series(sales,index=products)
+print(sales_series)
+
+print(sales_series['grapes'])
+
+total_sales = sales_series.sum()
+print(total_sales)
+
+data = {
+    'Name' : ['aliance','bob','leon'],
+    'Age' : [13,12,17],
+    'City' : ['New York','London','Prishtina'],
+}
+
+# df= pd.DataFrame(data)
+# print(df)
+
+# df =  pd.read_csv('cs.csv')
+
+# df.to_csv('dion.csv', index = False)
+
+best_selling_product = sales_series.idxmax()
+print(f"Best selling product:{best_selling_product}")
